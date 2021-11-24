7554d28ac76ede86e400531bc93b35990ca1f7df
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-11-24 12:29:37 +0900

    Install the necessary dependencies for pwa-asset-generator

--- install.sh
+++ install.sh
@@ -65,6 +65,48 @@ install_apt_packages() {
   # Install the build-essential package, which is required to install Homebrew
   sudo apt -Vy install build-essential
 
+  # Install the necessary dependencies for pwa-asset-generator
+  # see: https://github.com/onderceylan/pwa-asset-generator/issues/631
+  # see: https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#chrome-headless-doesnt-launch-on-unix
+  sudo apt -Vy install \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libc6 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libexpat1 \
+    libfontconfig1 \
+    libgbm1 \
+    libgcc1 \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libstdc++6 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libxrender1 \
+    libxss1 \
+    libxtst6 \
+    lsb-release \
+    wget \
+    xdg-utils
+
   # Upgrade Vim
   sudo add-apt-repository -y ppa:jonathonf/vim
   sudo apt -Vy upgrade vim
