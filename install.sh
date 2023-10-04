5c3117d6cc06e25ca167729d62e14456a4a4a5dd
Yuta Nagamiya <y.nagamiya@gmail.com>
2023-10-04 18:22:28 +0900

    Fix download URL for ngrok

--- install.sh
+++ install.sh
@@ -152,9 +152,9 @@ install_homebrew_packages() {
 
 install_archived_packages_for_wsl2() {
   # Install ngrok
-  curl -OfsSL --output-dir "${HOME}/tmp" https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz
-  tar zxvf "${HOME}/tmp/ngrok-stable-linux-arm64.tgz" -C "${HOME}/bin"
-  rm "${HOME}/tmp/ngrok-stable-linux-arm64.tgz"
+  curl -OfsSL --output-dir "${HOME}/tmp" https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+  tar zxvf "${HOME}/tmp/ngrok-stable-linux-amd64.tgz" -C "${HOME}/bin"
+  rm "${HOME}/tmp/ngrok-stable-linux-amd64.tgz"
   local ngrok_auth_token
   read -p 'Enter your ngrok auth token. (see: https://dashboard.ngrok.com/get-started/your-authtoken)' ngrok_auth_token
   ngrok authtoken "${ngrok_auth_token}"
