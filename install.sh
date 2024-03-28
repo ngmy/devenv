e8b7309c470ca92d684a66e978f16e06459d6f98
Yuta Nagamiya <y.nagamiya@gmail.com>
2024-03-28 19:02:56 +0900

    Install the Git Large File Storage

--- install.sh
+++ install.sh
@@ -134,6 +134,9 @@ install_apt_packages() {
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
   echo 'deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main' | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   sudo apt -Vy install google-cloud-cli
+
+  # Install the Git Large File Storage
+  sudo apt -Vy install git-lfs
 }
 
 install_homebrew_packages() {
